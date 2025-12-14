01f3ef0d6992132f6d804ffe1d3736eb193859ee
Safa Maqbool <s.maq.basha@gmail.com>
2025-12-14 21:41:18 +0500

    chore: update dotenv to version 16.6.1 and add Google OAuth setup in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,13 @@
+import "dotenv/config";
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+
 import express from "express";
 import cors from "cors";
+
+console.log("CLIENT_ID:", CLIENT_ID ? "Loaded" : "Missing");
+console.log("CLIENT_SECRET:", CLIENT_SECRET ? "Loaded" : "Missing");
 
 const app = express();
 app.use(cors());
@@ -8,6 +16,14 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.get("/auth/google", (req, res) => {
+  res.json({
+    message: "This will handle Google OAuth in the next step",
+    clientId: CLIENT_ID, // safe to show
+  });
+});
+
 
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
